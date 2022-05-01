85a4b6344ab841313e7818e61e17ad5c0936819e
Дмитрий <1917-01_1917@mail.ru>
2022-05-01 09:37:08 +0400

    First commit

---
+++ src/test/java/PostmanApiTest.java
@@ -0,0 +1,29 @@
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PostmanApiTest {
+
+    @Test
+    public void shouldReturnPostRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Здравствуйте, вас приветствует автоответчик," +
+                        "пожалуйста, оставьте сообщение после звукового сигнала.") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Здравствуйте, вас приветствует автоответчик," +
+                        "пожалуйста, оставьте сообщение после звукового сигнала."));
+    }
+
+}
